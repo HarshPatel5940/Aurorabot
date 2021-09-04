45ca88fa7e908fb17b47c94a75625be07c60989d
harshpatel5940 <harshpatel5940>
2021-09-04 17:28:12 +0530

    V8.7 proper dm embed

--- cogs/invites.py
+++ cogs/invites.py
@@ -39,26 +39,41 @@ class Invites(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         inviter = await self.tracker.fetch_inviter(member)  # inviter is the member who invited
-         
+
         if inviter is None:
             inviter.name = "Inviter Not Found"
-            inviter.discriminator = " Link deleted " 
-        
+            inviter.discriminator = " Link deleted "
+
         channel = self.client.get_channel(799978267293646868)
         guild = self.client.get_guild(799974967504535572)
-        embed = discord.Embed(
+        embed1 = discord.Embed(
             title=f"Welcome To FRNz Official Server.",
             description=f"**Member :** {member.mention}\n **Invited by: {inviter.name}#{inviter.discriminator}**",
             color=0x14a00f,
             timestamp=datetime.now(self.IST)
         )
-        embed.set_thumbnail(url=member.avatar.url)
-        embed.set_author(name=member.name, icon_url=member.avatar.url)
-        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon.url)
+        embed1.set_thumbnail(url=member.avatar.url)
+        embed1.set_author(name=member.name, icon_url=member.avatar.url)
+        embed1.set_footer(text=member.guild.name, icon_url=member.guild.icon.url)
+        x1 = f"{member.mention} Welcome to our FRNz Official community"
+        x2 = f"""
+<a:Heart:815506676533690390>You can promote your channel, videos  or blog in <#800291409726144513>
+<a:Heart:815506676533690390>Remember to Read Rules in <#799978440492449842>
+<a:Heart:815506676533690390>Take Self roles in <#799978698064265226>
+<a:Heart:815506676533690390>Have a great time here in {guild.name} """
+        embed2 = discord.Embed(
+            title="Welcome To FRNz Official Server",
+            description=x2,
+            color=0x17ffd9,
+            timestamp=datetime.now(self.IST)
+        )
+        embed2.set_thumbnail(url=member.guild.icon.url)
+        embed2.set_author(name=member.name, icon_url=member.avatar.url)
+        embed2.set_footer(text=member.guild.name, icon_url=member.guild.icon.url)
 
-        await member.send(f"Hi {member.mention} Welcome to our FRNz Official community.\n<a:Heart:815506676533690390>You can promote your channel, videos  or blog in <#800291409726144513>\n<a:Heart:815506676533690390>Remember to Read Rules in <#799978440492449842>\n<a:Heart:815506676533690390>Take Self roles in <#799978698064265226>\n\n<a:Heart:815506676533690390>Have a great time here in {guild.name}.")
+        await member.send(x1, embed=embed2)
         await asyncio.sleep(1)
-        await channel.send(embed=embed)
+        await channel.send(embed=embed1)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
